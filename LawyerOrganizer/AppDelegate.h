0b8cd737b2d7615a92f9f66b6ce6871514067d88
aevans002 <allanevans2005@msn.com>
2015-05-24 11:27:30 -0600

    Initial Commit

---
+++ LawyerOrganizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LawyerOrganizer
+//
+//  Created by Administrator on 5/24/15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
